cb4f8b5825d58bfc9b91c9453779f9f9f1a4adad
skdestination <skdestination01@gmail.com>
2026-06-06 07:39:46 +0530

    chore(android): refactor and clean up project config
    - Reset app versioning
    - Update Gradle build tools and Java compatibility
    - Remove redundant permissions and dependencies
    - Clean up manifest, styles, and MainActivity boilerplate

--- android/app/src/main/java/com/litecut/app/MainActivity.java
+++ android/app/src/main/java/com/litecut/app/MainActivity.java
@@ -1,32 +1,5 @@
 package com.litecut.app;
 
-import android.os.Bundle;
-import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(VideoProcessorPlugin.class);
-        super.onCreate(savedInstanceState);
-        hideSystemUI();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        hideSystemUI();
-    }
-
-    private void hideSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_FULLSCREEN
-        );
-    }
-}
+public class MainActivity extends BridgeActivity {}
